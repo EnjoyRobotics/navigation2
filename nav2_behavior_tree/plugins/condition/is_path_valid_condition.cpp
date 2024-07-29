a64592127c8fbce3cc5b1917211c4b46cfe05326
emilnovak <emil.tibor.novak@gmail.com>
2024-07-29 10:55:09 +0000

    Add service_name input port ot is_path_valid

--- nav2_behavior_tree/plugins/condition/is_path_valid_condition.cpp
+++ nav2_behavior_tree/plugins/condition/is_path_valid_condition.cpp
@@ -26,7 +26,6 @@ IsPathValidCondition::IsPathValidCondition(
 : BT::ConditionNode(condition_name, conf)
 {
   node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
-  client_ = node_->create_client<nav2_msgs::srv::IsPathValid>("is_path_valid");
 
   server_timeout_ = config().blackboard->template get<std::chrono::milliseconds>("server_timeout");
 }
@@ -34,6 +33,9 @@ IsPathValidCondition::IsPathValidCondition(
 void IsPathValidCondition::initialize()
 {
   getInput<std::chrono::milliseconds>("server_timeout", server_timeout_);
+
+  getInput("service_name", service_name_);
+  client_ = node_->create_client<nav2_msgs::srv::IsPathValid>(service_name_);
 }
 
 BT::NodeStatus IsPathValidCondition::tick()
