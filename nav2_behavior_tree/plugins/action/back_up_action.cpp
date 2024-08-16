765449b908f22fbaebbb171c0c09d0268a844230
redvinaa <redvinaa@gmail.com>
2024-08-16 16:15:56 +0200

    Read ports from on_tick

--- nav2_behavior_tree/plugins/action/back_up_action.cpp
+++ nav2_behavior_tree/plugins/action/back_up_action.cpp
@@ -24,25 +24,25 @@ BackUpAction::BackUpAction(
   const std::string & xml_tag_name,
   const std::string & action_name,
   const BT::NodeConfiguration & conf)
-: BtActionNode<nav2_msgs::action::BackUp>(xml_tag_name, action_name, conf)
-{
-  double dist;
-  getInput("backup_dist", dist);
-  double speed;
-  getInput("backup_speed", speed);
-  double time_allowance;
-  getInput("time_allowance", time_allowance);
-
-  // Populate the input message
-  goal_.target.x = dist;
-  goal_.target.y = 0.0;
-  goal_.target.z = 0.0;
-  goal_.speed = speed;
-  goal_.time_allowance = rclcpp::Duration::from_seconds(time_allowance);
-}
+: BtActionNode<nav2_msgs::action::BackUp>(xml_tag_name, action_name, conf) {}
 
 void BackUpAction::on_tick()
 {
+  if (status() == BT::NodeStatus::IDLE) {
+    double dist;
+    getInput("backup_dist", dist);
+    double speed;
+    getInput("backup_speed", speed);
+    double time_allowance;
+    getInput("time_allowance", time_allowance);
+
+    // Populate the input message
+    goal_.target.x = dist;
+    goal_.target.y = 0.0;
+    goal_.target.z = 0.0;
+    goal_.speed = speed;
+    goal_.time_allowance = rclcpp::Duration::from_seconds(time_allowance);
+  }
   increment_recovery_count();
 }
 
